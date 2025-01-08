0be93c9a8d344e7b734236860a35bf5af018a643
Nate Z <ntzeitlin@gmail.com>
2025-01-08 10:40:46 -0600

    fixed namechange mislink

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomersList"
-import { EmployeeList } from "./components/employee/EmployeeList"
+import { EmployeeList } from "./components/employees/EmployeeList"
 import { TicketList } from "./components/tickets/TicketList"
 
 export const App = () => {
