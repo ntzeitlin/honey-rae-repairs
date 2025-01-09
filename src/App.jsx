97ae0574d315f60185e35bf190258e09187e55f9
Nate Z <ntzeitlin@gmail.com>
2025-01-09 09:58:20 -0600

    implemented navbar with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { Routes, Route, Outlet } from "react-router-dom"
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomersList"
 import { EmployeeList } from "./components/employees/EmployeeList"
 import { TicketList } from "./components/tickets/TicketList"
+import { NavBar } from "./components/nav/NavBar"
 
 export const App = () => {
-  return <>
-    <EmployeeList />
-    <CustomerList />
-    <TicketList />
-  </>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }>
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+        <Route path="employees" element={<EmployeeList />} />
+      </Route>
+    </Routes>
+  )
 }
