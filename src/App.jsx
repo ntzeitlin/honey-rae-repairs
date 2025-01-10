ad44cb6fdb35174f625a617ab36194bba128f7d3
Nate Z <ntzeitlin@gmail.com>
2025-01-10 09:13:37 -0600

    added authorized application views

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import { Routes, Route } from "react-router-dom"
 import "./App.css"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
+import { Authorized } from "./views/Authorized"
+import { ApplicationViews } from "./views/ApplicationViews"
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={
+        // check if user is authorized
+        <Authorized>
+
+          {/* ApplicationViews is the child component of Authorized. 
+          If Authorized finds the user login object in local storage, 
+          then allow access to the application view. */}
+          <ApplicationViews />
+
+        </Authorized>
+      } />
     </Routes>
   )
 }
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,7 +1,9 @@
 import "./NavBar.css"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export const NavBar = () => {
+    const navigate = useNavigate
     return (
         <ul className="navbar">
             <li className="navbar-item">
@@ -13,6 +15,21 @@ export const NavBar = () => {
             <li className="navbar-item">
                 <Link to='/customers'>Customers</Link>
             </li>
+            {localStorage.getItem("honey_user") ? (
+                <li className="navbar-item navbar-logout">
+                    <Link className="navbar-link"
+                        to=""
+                        onClick={() => {
+                            localStorage.removeItem("honey_user")
+                            navigate("/", { replace: true })
+                        }}
+                    >
+                        Logout
+                    </Link>
+                </li>
+            ) : (
+                ""
+            )}
         </ul>
     )
 }
