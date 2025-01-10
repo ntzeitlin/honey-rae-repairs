504e593fae6449682bec805899e72caaa695e935
Nate Z <ntzeitlin@gmail.com>
2025-01-10 11:18:23 -0600

    refactored the onchange functions

--- src/components/forms/EmployeeForm.jsx
+++ src/components/forms/EmployeeForm.jsx
@@ -32,6 +32,12 @@ export const EmployeeForm = ({ currentUser }) => {
         })
     }
 
+    const handleInputChange = (event) => {
+        const stateCopy = { ...employee }
+        stateCopy[event.target.name] = event.target.value
+        setEmployee(stateCopy)
+    }
+
     return (
         <form className="profile">
             <h2>Update Profile</h2>
@@ -40,12 +46,9 @@ export const EmployeeForm = ({ currentUser }) => {
                     <label>Specialty:</label>
                     <input
                         type="text"
-                        value={employee.specialty}
-                        onChange={(event) => {
-                            const copy = { ...employee }
-                            copy.specialty = event.target.value
-                            setEmployee(copy)
-                        }}
+                        name="specialty"
+                        value={employee.specialty ? employee.specialty : ''}
+                        onChange={handleInputChange}
                         required
                         className="form-control" />
                 </div>
@@ -55,12 +58,9 @@ export const EmployeeForm = ({ currentUser }) => {
                     <label>Hourly Rate:</label>
                     <input
                         type="number"
-                        value={employee.rate}
-                        onChange={(event) => {
-                            const copy = { ...employee }
-                            copy.rate = event.target.value
-                            setEmployee(copy)
-                        }}
+                        name="rate"
+                        value={employee.rate ? employee.rate : 0}
+                        onChange={handleInputChange}
                         required
                         className="form-control" />
                 </div>
